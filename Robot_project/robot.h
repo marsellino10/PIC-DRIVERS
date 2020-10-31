09b448a700a252f8da683a11c8d19222fcc08c1b
marsellino10 <54001585+marsellino10@users.noreply.github.com>
2020-10-31 18:49:06 +0200

    Create robot.h

---
+++ Robot_project/robot.h
@@ -0,0 +1,17 @@
+/* 
+ * File:   application.h
+ * Author: marsellino medhat
+ * Created on October 31, 2020, 12:55 PM
+ */
+
+#ifndef ROBOT_H
+#define	ROBOT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "device_config.h"
+
+#include "ecu/DC_motor/DC_motor.h"
+
+#endif	/* ROBOT_H */
